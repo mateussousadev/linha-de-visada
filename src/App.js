d9cde13514bff17e89e3fce50df80e526cc83051
Mateus Sousa <sousamateus443@gmail.com>
2020-10-12 21:27:00 -0300

    Deploy the Calc Function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HeaderLogo from "./assets/antenna.svg";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FormCalc from './Components/FormCalc';
 
 
 function App() {
@@ -10,26 +11,11 @@ function App() {
       <section id="main" className="container-fluid">
         <div className="card card-main p-5 m-2">
           <div className="header mb-4 text-center">
-            <img src={HeaderLogo}></img>
+            <img alt="Icone de uma antena" src={HeaderLogo}></img>
             <h2>Linha de Visada Online</h2>
           </div>
-          <form className="formInputs">
-            <div className="form-group">
-              <label for="exampleFormControlInput1">Antena01</label>
-              <input type="number" className="form-control" id="antena01" placeholder="altura em metros"></input>
-            </div>
-
-            <div className="form-group">
-              <label for="exampleFormControlInput1">Antena02</label>
-              <input type="number" className="form-control" id="antena02" placeholder="altura em metros"></input>
-            </div>
-            <button type="submit" className="btn w-100 mb-2">Calcular</button>
-          </form>
-          <h6 className="card-description mt-2">Resultado</h6>
-
-          <div className=" resultado text-center">
-            <h4 className="p-4">23km</h4>
-          </div>
+           {/* Aqui é o componente que tem os forms */}
+          <FormCalc></FormCalc>
         </div>  
         <footer className="footer">
           <div className="pt-2">
---
+++ src/Components/FormCalc.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Form,Button} from "react-bootstrap";
+import ReloadIcon from "../assets/reload.svg";
+
+
+export default class FormCalc extends React.Component {
+    state = {
+        feed: [],
+        antena01: "",
+        antena02: "",
+        resultado: "0",
+    };
+    
+    calc = () => {
+        const{ antena01, antena02 } = this.state;
+        let result = ((3.56 * parseFloat(antena01))+ (3.56 * parseFloat(antena02)))
+        this.setState({resultado: result})
+        
+    };
+    reset  = async () =>{
+        await this.setState({antena01: "0", antena02: "0", resultado: "0"})
+        console.log(this.state.antena01, this.state.antena02)
+    }
+    
+    handleChange = (e) => {
+        // console.log(e.target.name);
+        this.setState({ [e.target.name]: e.target.value });
+      };
+
+    render() {
+        return (
+            <div>
+                <div className="formInputs">
+                    <Form.Group controlId="antena01">
+                        <Form.Label>Antena 01</Form.Label>
+                        <Form.Control name="antena01" type="number"  value={this.state.antena01}  placeholder="altura em metros" onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Form.Group controlId="antena02">
+                        <Form.Label>Antena 02</Form.Label>
+                        <Form.Control name="antena02" type="number" value={this.state.antena02}  placeholder="altura em metros" onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Button className="btn w-100 mb-2"  variant="primary"  type="submit" onClick={this.calc}>Calcular</Button>
+                </div>
+                <Form.Group controlId="resultado" className="text-center" >
+                    <Form.Label>Resultado em KM:</Form.Label>
+                    <div className="resultado py-2">
+                        
+                        <Form.Text >{this.state.resultado} km</Form.Text>
+                    </div>
+                    
+                    <Button onClick={this.reset}className="mt-3">
+                        <img alt="" width="30px" src={ReloadIcon}></img>
+                    </Button>
+                </Form.Group>
+            </div>
+        );
+    }
+}
